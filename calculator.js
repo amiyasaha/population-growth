9ecf29b350a9b26e632d0fa6c2aa481c2d68f845
amiyasaha <developeramiya@gmail.com>
2025-09-08 17:27:57 -0700

    mass population data + iterative functionality

--- calculator.js
+++ calculator.js
@@ -6,12 +6,49 @@ function logisticProjection(pop, rate, time, cap) {
     return  cap / (1+((cap-pop) / pop) * Math.exp(-rate / 100 * time))
 }
 
+function iterativeExponential(pop, rate, time, result) {
+    for(let i = 0; i <= time; i++) {
+        result.innerHTML += exponentialProjection(pop, rate, i) + "<br>";
+    }
+}
+
+function iterativeLogistic() {
+    for(let i = 0; i <= time; i++) {
+        result.innerHTML += logisticProjection(pop, rate, i, cap) + "<br>";
+    }
+}
+
+function exponentialYears() {
+
+}
+
+function logisticYears() {
+
+}
+
+
+// dealing with JSON @ https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/JSON
+async function populateCountries()  {
+    const request = new Request('country-by-population.json');
+    const response = await fetch(request); 
+    const countries = await response.json();
+
+    let populations = document.getElementById("countries");
+
+    countries.forEach(function(country) {
+        populations.innerHTML += "<option value='" + country.population + "'>" + country.country + " (" + country.population +  ") </option>";
+    });
+
+}
+
 window.onload = function () {
     let button = document.getElementById("calculate");
     let result = document.getElementById("result");
     
     let mode = document.getElementById("mode");
     let cap = document.getElementById("capacity");
+
+    populateCountries();
 
     mode.onchange = function() {
         if (mode.value == "logistic") {
@@ -33,8 +70,7 @@ window.onload = function () {
 
 
         if (current == "exponential") {
-            let expPop = exponentialProjection(country, rate, time);
-            result.innerHTML = expPop;
+            let expPop = iterativeExponential(country, rate, time, result);
             
         }
 
